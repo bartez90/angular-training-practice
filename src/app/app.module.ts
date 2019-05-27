7ba3ac8f96c2c71226222d01f05d97e25a3c6527
bartez90 <mojcin1@o2.pl>
2019-05-27 12:48:11 +0200

    1-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooksOrdersComponent } from './books-orders/books-orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksOrdersComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/books-orders/books-orders.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-books-orders',
+  templateUrl: './books-orders.component.html',
+  styleUrls: ['./books-orders.component.scss']
+})
+export class BooksOrdersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
