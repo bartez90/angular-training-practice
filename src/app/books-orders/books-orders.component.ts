85e75c1c982ad12b08e5213b93fd435fc2aa81a3
bartez90 <mojcin1@o2.pl>
2019-05-27 13:36:24 +0200

    2-data-binding

--- src/app/books-orders/books-orders.component.ts
+++ src/app/books-orders/books-orders.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-books-orders',
-  templateUrl: './books-orders.component.html',
-  styleUrls: ['./books-orders.component.scss']
+    selector: 'app-books-orders',
+    templateUrl: './books-orders.component.html',
+    styleUrls: ['./books-orders.component.scss']
 })
 export class BooksOrdersComponent implements OnInit {
+    booksCount = 2;
+    noBooks = true;
+    defaultHeading = 'Books orders:';
+    secondHeading = 'Some awesome functionality will be here...';
+    heading = this.defaultHeading;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    changeHeading() {
+        this.heading = this.heading === this.defaultHeading ? this.secondHeading : this.defaultHeading;
+    }
 }
